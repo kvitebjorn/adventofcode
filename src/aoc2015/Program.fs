383bccdda3ea8d460178f4e3e6ce1df92f2510e3
kvitebjorn <harintonkairu@gmail.com>
2023-11-14 15:31:48 -0700

    day 1 solutions

--- src/aoc2015/Program.fs
+++ src/aoc2015/Program.fs
@@ -1,7 +1,25 @@
 ﻿module Program
 
+// TODO: there's gotta be a way to do this better...
+let solutions =
+   Map.empty.
+      Add("Day1.answerPt1", Day1.answerPt1).
+      Add("Day1.answerPt2", Day1.answerPt2);;
+
 [<EntryPoint>]
 let main args =
-    for arg in args do
-        printfn "TODO"
+    let mutable day = ""
+    let mutable part = ""
+    try
+        day  <- args.[0]
+        part <- args.[1]
+    with
+        | _ -> failwith "Invalid args, expected {day} {part}"
+
+    let solutionToGet = $"Day{day}.answerPt{part}"
+
+    try
+        printfn $"{solutions[solutionToGet]}"
+    with
+        | _ -> printfn $"No solution for day {day} part {part}"
     0
--- src/aoc2015/day01.fs
+++ src/aoc2015/day01.fs
@@ -1,10 +1,20 @@
 module Day1
 
-let rec solve input pos =
+let rec solvePt1 input pos =
     match input with 
-    | '('::tail -> solve tail (pos + 1)
-    | ')'::tail -> solve tail (pos - 1)
+    | '('::tail -> solvePt1 tail (pos + 1)
+    | ')'::tail -> solvePt1 tail (pos - 1)
     | _         -> pos
 
-let input = "((("
-let answer = solve (input |> Seq.toList) 0
+let input = System.IO.File.ReadAllText "day01.txt"
+let answerPt1 = solvePt1 (input |> Seq.toList) 0
+
+let rec solvePt2 input pos idx =
+    if pos = -1 then
+        idx
+    else
+        match input with 
+        | '('::tail -> solvePt2 tail (pos + 1) (idx + 1)
+        | ')'::tail -> solvePt2 tail (pos - 1) (idx + 1)
+        | _         -> pos
+let answerPt2 = solvePt2 (input |> Seq.toList) 0 0
