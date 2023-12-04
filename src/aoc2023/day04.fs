cfecc563abbdfbf433c8b01476315522a30ae358
kvitebjorn <harintonkairu@gmail.com>
2023-12-04 00:27:09 -0700

    solve day 4

---
+++ src/aoc2023/day04.fs
@@ -0,0 +1,34 @@
+module Day4
+
+open System.IO
+open System.Text.RegularExpressions
+
+let raw = File.ReadAllLines "day04.txt"
+
+let rec score n = 
+    match n with
+    | 0 | 1  -> n
+    | n      -> score (n - 1) * 2
+
+let getWinners card =
+    let matched = Regex.Match(card, @".*: ([\d\s]*) \| ([\d\s]*)")
+    let winning = Set(matched.Groups[1].Value.Split(" ") |> Array.filter (fun s -> s <> ""))
+    let actual  = Set(matched.Groups[2].Value.Split(" ") |> Array.filter (fun s -> s <> ""))
+    Set.intersect winning actual |> Set.count
+
+let solvePt1 input =
+    let total = Seq.map (fun c -> score(getWinners c)) input |> Seq.sum
+    $"{total.ToString()}"
+
+let answerPt1 = solvePt1 raw
+
+let solvePt2 input =
+    let winners = List.mapi (fun i c -> (i, getWinners c)) input
+    let copies = Array.create input.Length 1
+    for (i, numWon) in winners do
+        for j = i + 1 to i + numWon do
+            copies[j] <- copies[j] + 1 * copies[i]
+    let total = Array.sum copies
+    $"{total.ToString()}"
+
+let answerPt2 = solvePt2 (raw |> Seq.toList)
