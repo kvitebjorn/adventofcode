1cc703345e97adcb1286fde9d7123d5c9495d276
kvitebjorn <harintonkairu@gmail.com>
2023-12-05 01:28:29 -0700

    move common code out

--- src/aoc2023/day05.fs
+++ src/aoc2023/day05.fs
@@ -11,8 +11,25 @@ let split s =
         | _  -> a::cur, acc
     let result = Array.foldBack folder s ([Array.last s], []) 
     (fst result)::(snd result)
+let generateSeeds seedRanges =
+    seedRanges
+    |> List.chunkBySize 2
+    |> List.map (fun p -> seq { p[0]..p[0]+p[1] } |> Seq.toList)
+    |> List.reduce List.append
 let stringToInts (s: string) = List.map (fun x -> x |> int64) (s.Split(" ") |> Array.toList)
 let parseMap (m: list<string>) = List.map stringToInts m[1..]
+let seeds = List.map (fun x -> x |> int64) (raw[0].Replace("seeds: ", "").Split(" ") |> Array.toList)
+let seedRanges = 
+    List.map (fun x -> x |> int64) (raw[0].Replace("seeds: ", "").Split(" ") |> Array.toList)
+let seedsPt2 = generateSeeds seedRanges
+let maps  = split raw[2..]
+let seedToSoilMaps = parseMap maps[0]
+let soilToFertilizerMaps = parseMap maps[1]
+let fertilizerToWaterMaps = parseMap maps[2]
+let waterToLightMaps = parseMap maps[3]
+let lightToTemperatureMaps = parseMap maps[4]
+let temperatureToHumidityMaps = parseMap maps[5]
+let humidityToLocationMaps = parseMap maps[6]
 let sourceToDest (maps: list<list<int64>>) (source: int64) =
     let isBetween (n: int64) (map: list<int64>) = 
         let lower = map[1]
@@ -25,13 +42,6 @@ let sourceToDest (maps: list<list<int64>>) (source: int64) =
         let m = List.head map
         source - (m[1] - m[0])
 let seedToLocation
-    seedToSoilMaps
-    soilToFertilizerMaps
-    fertilizerToWaterMaps
-    waterToLightMaps
-    lightToTemperatureMaps
-    temperatureToHumidityMaps
-    humidityToLocationMaps
     seed =
     seed |>
     sourceToDest seedToSoilMaps |>
@@ -42,60 +52,14 @@ let seedToLocation
     sourceToDest temperatureToHumidityMaps |>
     sourceToDest humidityToLocationMaps
     
-let solvePt1 (input: string array) =
-    let seeds = List.map (fun x -> x |> int64) (input[0].Replace("seeds: ", "").Split(" ") |> Array.toList)
-    let maps  = split input[2..]
-    let seedToSoilMaps = parseMap maps[0]
-    let soilToFertilizerMaps = parseMap maps[1]
-    let fertilizerToWaterMaps = parseMap maps[2]
-    let waterToLightMaps = parseMap maps[3]
-    let lightToTemperatureMaps = parseMap maps[4]
-    let temperatureToHumidityMaps = parseMap maps[5]
-    let humidityToLocationMaps = parseMap maps[6]
-    let seedToLocationPartial seed = 
-        seedToLocation     
-            seedToSoilMaps
-            soilToFertilizerMaps
-            fertilizerToWaterMaps
-            waterToLightMaps
-            lightToTemperatureMaps
-            temperatureToHumidityMaps
-            humidityToLocationMaps 
-            seed
-    let lowest = List.map seedToLocationPartial seeds |> List.min
+let solvePt1 =
+    let lowest = List.map seedToLocation seeds |> List.min
     $"{lowest.ToString()}"
 
-let answerPt1 = solvePt1 raw
+let answerPt1 = solvePt1
 
-let generateSeeds seedRanges =
-    seedRanges
-    |> List.chunkBySize 2
-    |> List.map (fun p -> seq { p[0]..p[0]+p[1] } |> Seq.toList)
-    |> List.reduce List.append
-
-let solvePt2 (input: string array) =
-    let seedRanges = 
-        List.map (fun x -> x |> int64) (input[0].Replace("seeds: ", "").Split(" ") |> Array.toList)
-    let seeds = generateSeeds seedRanges
-    let maps  = split input[2..]
-    let seedToSoilMaps = parseMap maps[0]
-    let soilToFertilizerMaps = parseMap maps[1]
-    let fertilizerToWaterMaps = parseMap maps[2]
-    let waterToLightMaps = parseMap maps[3]
-    let lightToTemperatureMaps = parseMap maps[4]
-    let temperatureToHumidityMaps = parseMap maps[5]
-    let humidityToLocationMaps = parseMap maps[6]
-    let seedToLocationPartial seed = 
-        seedToLocation     
-            seedToSoilMaps
-            soilToFertilizerMaps
-            fertilizerToWaterMaps
-            waterToLightMaps
-            lightToTemperatureMaps
-            temperatureToHumidityMaps
-            humidityToLocationMaps 
-            seed
-    let lowest = List.map seedToLocationPartial seeds |> List.min
+let solvePt2 =
+    let lowest = List.map seedToLocation seedsPt2 |> List.min
     $"{lowest.ToString()}"
 
-let answerPt2 = solvePt2 raw
+let answerPt2 = solvePt2
