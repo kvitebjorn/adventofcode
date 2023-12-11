b490332a01d12ca8e35991836098e73e9de08247
kvitebjorn <harintonkairu@gmail.com>
2023-12-10 22:00:28 -0700

    solve day 10 part 2

--- aoc2023/day10.fsx
+++ aoc2023/day10.fsx
@@ -19,9 +19,7 @@ for i in [0..(input.Length - 1)] do
                 sx <- j
                 sy <- i
         | _ -> ()
-        match c with
-        | '.' -> ()
-        | _   -> g.AddVertex((j, i, c)) |> ignore
+        g.AddVertex((j, i, c)) |> ignore
 let up   = ['|';'7';'F';'S']
 let down = ['|';'L';'J';'S']
 let left = ['-';'L';'F';'S']
@@ -30,7 +28,7 @@ let isAdjacent (x1,y1,c1) (x2,y2,c2) =
     let dx = abs(x1 - x2)
     let dy = abs(y1 - y2)
     let isNextTo = dx + dy = 1
-    if isNextTo then
+    if isNextTo && c1 <> '.' && c2 <> '.' then
         let isUp    = y1 - 1 = y2 && List.contains c2 up && List.contains c1 down
         let isDown  = y1 + 1 = y2 && List.contains c2 down && List.contains c1 up
         let isLeft  = x1 - 1 = x2 && List.contains c2 left && List.contains c1 right
@@ -48,19 +46,24 @@ let distObserver = new VertexDistanceRecorderObserver<TVertex, TEdge>((fun x -> 
 distObserver.Attach(dijkstra);
 let pathObserver = new VertexPredecessorRecorderObserver<TVertex, TEdge>()
 pathObserver.Attach(dijkstra);
-let solvePt1 =
-    dijkstra.Compute(TVertex(sx, sy, 'S'))
-    let farthest = Seq.last distObserver.Distances.Values
-    $"{farthest}"
-let answerPt1 = solvePt1 
-printfn "%s" answerPt1
+dijkstra.Compute(TVertex(sx, sy, 'S'))
+let farthest = Seq.last distObserver.Distances.Values 
+printfn $"{farthest}"
 
-let solvePt2 =
-    """
-        TODO: 
-            1. get the main loop for 'S'; 
-            2. do the same algorithm i did in 2015 on HEERD
-                to identify pixels that had to be shaded in a target area
-    """
-let answerPt2 = solvePt2 
-printfn "%s" answerPt2
+let allNodes = Set(g.Vertices)
+let mutable loopNodes = Set(pathObserver.VertexPredecessors.Keys)
+loopNodes <- loopNodes.Add(TVertex(sx, sy, 'S'))
+let vx (x, _, _) = x
+let vy (_, y, _) = y
+let boundaryLoopNodes = Set.filter (fun n -> 
+            let c = input[vy n][vx n]
+            match c with
+            | '|' | '7' | 'F' | 'S' -> true
+            | _ -> false) loopNodes
+let nonLoopNodes = Set.difference allNodes loopNodes
+let isEnclosed n =
+    let intersections = Set.filter (fun b -> vx b > vx n && vy b = vy n) boundaryLoopNodes
+    intersections.Count % 2 = 1
+let enclosed = Set.filter isEnclosed nonLoopNodes
+
+printfn "%s" $"{enclosed.Count}"
